07c286fcff5fadcf351b1dc9d27cbc5615630ecd
amanthokale <amanthokale1137@gmail.com>
2022-05-18 18:47:29 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,11 +4,22 @@ import clothes from '../clothes.json';
 
 export default function App() {
   const [cat, setcat] = useState('all');
-
-  const set = (a) => {
+  const [search, setsearch] = useState('');
+  const [l, setl] = useState(0);
+  const [r, setr] = useState(100);
+  const setf = (a) => {
     console.log(a);
     setcat(a);
   };
+
+  const searchf = (a) => {
+    setcat('none');
+    setsearch(a);
+  };
+  const addfilter = (a) => {
+    setcat('none');
+  };
+
   return (
     <>
       <div className="head">
@@ -18,17 +29,23 @@ export default function App() {
       </div>
       <div className="search">
         <div>
-          <input type="search" placeholder="Search for products" />
+          <input
+            onChange={(e) => {
+              searchf(e.target.value);
+            }}
+            type="search"
+            placeholder="Search for products"
+          />
           <input id="btn" type="button" value="search" />
         </div>
       </div>
 
       <div class="categories">
         <ul>
-          <li onClick={(e) => set('all')}>All</li>
-          <li onClick={(e) => set('top')}>Topwear</li>
-          <li onClick={(e) => set('bottom')}>Bottomwear</li>
-          <li onClick={(e) => set('acc')}>Accessories</li>
+          <li onClick={(e) => setf('all')}>All</li>
+          <li onClick={(e) => setf('top')}>Topwear</li>
+          <li onClick={(e) => setf('bottom')}>Bottomwear</li>
+          <li onClick={(e) => setf('acc')}>Accessories</li>
         </ul>
       </div>
       <div className="bodysec">
@@ -36,10 +53,30 @@ export default function App() {
           <h2>Filter</h2>
           <div className="price">
             <h5>Price Range</h5>
-            <input type="text" />
+            <input
+              onChange={(e) => {
+                setl(e.target.value);
+                setcat('none');
+              }}
+              placeholder={l}
+              type="text"
+            />
             <h6>TO</h6>
-            <input type="text" />
-            <button>Add Filter</button>
+            <input
+              onChange={(e) => {
+                setr(e.target.value);
+                setcat('none');
+              }}
+              placeholder={r}
+              type="text"
+            />
+            <button
+              onClick={(e) => {
+                addfilter();
+              }}
+            >
+              Add Filter
+            </button>
           </div>
         </div>
         <div id="products">
@@ -48,8 +85,45 @@ export default function App() {
               .filter((val) => {
                 if (cat == 'all') {
                   return val;
-                  s;
                 } else if (val.category == cat) {
+                  return val;
+                }
+              })
+              .map((val, key) => {
+                return (
+                  <div class="item">
+                    <img src={val.img} width="100" height="100" />
+                    <p class="cent bold">{val.name}</p>
+                    <h6 class="cent bold">${val.price}</h6>
+                  </div>
+                );
+              })}
+          </div>
+
+          <div className="json">
+            {clothes
+              .filter((val) => {
+                if (search === '') {
+                  return;
+                }
+                if (val.name.toLowerCase().includes(search.toLowerCase())) {
+                  return val;
+                }
+              })
+              .map((val, key) => {
+                return (
+                  <div class="item">
+                    <img src={val.img} width="100" height="100" />
+                    <p class="cent bold">{val.name}</p>
+                    <h6 class="cent bold">${val.price}</h6>
+                  </div>
+                );
+              })}
+          </div>
+          <div className="json">
+            {clothes
+              .filter((val) => {
+                if (val.price > l && val.price < r) {
                   return val;
                 }
               })
